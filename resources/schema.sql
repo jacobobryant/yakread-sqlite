6be9edc8997d6087e064e5045806b4bad89a7bd9
Jacob O'Bryant <git@jacobobryant.com>
2026-02-01 09:03:52 -0700

    Fix up the sqlite schema

--- resources/schema.sql
+++ resources/schema.sql
@@ -1,9 +1,12 @@
+-- test:    `sqlite3def storage/sqlite/test.db --dry-run -f resources/schema.sql`
+-- migrate: `sqlite3def storage/sqlite/test.db --apply -f resources/schema.sql`
+
 CREATE TABLE user (
-  id BLOB PRIMARY KEY,
-  email TEXT,
-  roles BLOB,
+  id BLOB PRIMARY KEY NOT NULL,
+  email TEXT NOT NULL,
+  roles BLOB, -- [:set [:enum :admin]]
   joined_at INT,
-  digest_days BLOB,
+  digest_days BLOB, -- [:set ::day]
   send_digest_at TEXT,
   timezone TEXT,
   digest_last_sent INT,
@@ -16,140 +19,9 @@ CREATE TABLE user (
   cancel_at INT
 ) STRICT;
 
-CREATE TABLE sub_base (
-  id BLOB PRIMARY KEY,
-  user_id BLOB,
-  created_at INT,
-  pinned_at INT,
-  FOREIGN KEY(user_id) REFERENCES user(id)
-) STRICT;
-
-CREATE TABLE sub_feed (
-  id BLOB PRIMARY KEY,
-  user_id BLOB,
-  created_at INT,
-  pinned_at INT,
-  feed_id BLOB,
-  FOREIGN KEY(user_id) REFERENCES user(id),
-  FOREIGN KEY(feed_id) REFERENCES feed(id)
-) STRICT;
-
-CREATE TABLE sub_email (
-  id BLOB PRIMARY KEY,
-  user_id BLOB,
-  created_at INT,
-  pinned_at INT,
-  from_email TEXT,
-  unsubscribed_at INT,
-  FOREIGN KEY(user_id) REFERENCES user(id)
-) STRICT;
-
-CREATE TABLE item_base (
-  id BLOB PRIMARY KEY,
-  ingested_at INT,
-  title TEXT,
-  url TEXT,
-  redirect_urls BLOB,
-  content TEXT,
-  content_key BLOB,
-  published_at INT,
-  excerpt TEXT,
-  author_name TEXT,
-  author_url TEXT,
-  feed_url TEXT,
-  lang TEXT,
-  site_name TEXT,
-  byline TEXT,
-  length INT,
-  image_url TEXT,
-  paywalled INT
-) STRICT;
-
-CREATE TABLE item_feed (
-  id BLOB PRIMARY KEY,
-  ingested_at INT,
-  title TEXT,
-  url TEXT,
-  redirect_urls BLOB,
-  content TEXT,
-  content_key BLOB,
-  published_at INT,
-  excerpt TEXT,
-  author_name TEXT,
-  author_url TEXT,
-  feed_url TEXT,
-  lang TEXT,
-  site_name TEXT,
-  byline TEXT,
-  length INT,
-  image_url TEXT,
-  paywalled INT,
-  feed_id BLOB,
-  guid TEXT,
-  FOREIGN KEY(feed_id) REFERENCES feed(id)
-) STRICT;
-
-CREATE TABLE item_email (
-  id BLOB PRIMARY KEY,
-  ingested_at INT,
-  title TEXT,
-  url TEXT,
-  redirect_urls BLOB,
-  content TEXT,
-  content_key BLOB,
-  published_at INT,
-  excerpt TEXT,
-  author_name TEXT,
-  author_url TEXT,
-  feed_url TEXT,
-  lang TEXT,
-  site_name TEXT,
-  byline TEXT,
-  length INT,
-  image_url TEXT,
-  paywalled INT,
-  sub_id BLOB,
-  raw_content_key BLOB,
-  list_unsubscribe TEXT,
-  list_unsubscribe_post TEXT,
-  reply_to TEXT,
-  maybe_confirmation INT,
-  FOREIGN KEY(sub_id) REFERENCES sub_base(id)
-) STRICT;
-
-CREATE TABLE item_direct (
-  id BLOB PRIMARY KEY,
-  ingested_at INT,
-  title TEXT,
-  url TEXT,
-  redirect_urls BLOB,
-  content TEXT,
-  content_key BLOB,
-  published_at INT,
-  excerpt TEXT,
-  author_name TEXT,
-  author_url TEXT,
-  feed_url TEXT,
-  lang TEXT,
-  site_name TEXT,
-  byline TEXT,
-  length INT,
-  image_url TEXT,
-  paywalled INT,
-  doc_type INT CHECK (doc_type = 0), -- :item/direct
-  candidate_status INT CHECK (candidate_status IN (0, 1, 2)) -- :ingest-failed (0), :blocked (1), :approved (2)
-) STRICT;
-
-CREATE TABLE redirect (
-  id BLOB PRIMARY KEY,
-  url TEXT,
-  item_id BLOB,
-  FOREIGN KEY(item_id) REFERENCES item_base(id)
-) STRICT;
-
 CREATE TABLE feed (
-  id BLOB PRIMARY KEY,
-  url TEXT,
+  id BLOB PRIMARY KEY NOT NULL,
+  url TEXT NOT NULL,
   synced_at INT,
   title TEXT,
   description TEXT,
@@ -157,13 +29,66 @@ CREATE TABLE feed (
   etag TEXT,
   last_modified TEXT,
   failed_syncs INT,
-  moderation INT CHECK (moderation IN (0, 1)) -- :approved (0), :blocked (1)
+  moderation INT CHECK (moderation IN (0, 1)) -- ; :approved (0), :blocked (1)
+) STRICT;
+
+CREATE TABLE sub (
+  id BLOB PRIMARY KEY NOT NULL,
+  user_id BLOB NOT NULL,
+  created_at INT NOT NULL,
+  pinned_at INT,
+  record_type INT NOT NULL CHECK (record_type IN (0, 1, 2)),
+  feed_id BLOB,
+  email_from TEXT,
+  email_unsubscribed_at INT,
+  FOREIGN KEY(user_id) REFERENCES user(id),
+  FOREIGN KEY(feed_id) REFERENCES feed(id)
+) STRICT;
+
+CREATE TABLE item (
+  id BLOB PRIMARY KEY NOT NULL,
+  ingested_at INT NOT NULL,
+  title TEXT,
+  url TEXT,
+  redirect_urls BLOB, -- [:set :string]
+  content TEXT,
+  content_key BLOB,
+  published_at INT,
+  excerpt TEXT,
+  author_name TEXT,
+  author_url TEXT,
+  feed_url TEXT,
+  lang TEXT,
+  site_name TEXT,
+  byline TEXT,
+  length INT,
+  image_url TEXT,
+  paywalled INT,
+  record_type INT NOT NULL CHECK (record_type IN (0, 1, 2, 3)), -- :feed (0), :email (1), :direct (2)
+  feed_id BLOB,
+  feed_guid TEXT,
+  email_sub_id BLOB,
+  email_raw_content_key BLOB,
+  email_list_unsubscribe TEXT,
+  email_list_unsubscribe_post TEXT,
+  email_reply_to TEXT,
+  email_maybe_confirmation INT,
+  direct_candidate_status INT CHECK (direct_candidate_status IN (0, 1, 2)),
+  FOREIGN KEY(feed_id) REFERENCES feed(id),
+  FOREIGN KEY(email_sub_id) REFERENCES sub(id)
+) STRICT;
+
+CREATE TABLE redirect (
+  id BLOB PRIMARY KEY NOT NULL,
+  url TEXT NOT NULL,
+  item_id BLOB NOT NULL,
+  FOREIGN KEY(item_id) REFERENCES item(id)
 ) STRICT;
 
 CREATE TABLE user_item (
-  id BLOB PRIMARY KEY,
-  user_id BLOB,
-  item_id BLOB,
+  id BLOB PRIMARY KEY NOT NULL,
+  user_id BLOB NOT NULL,
+  item_id BLOB NOT NULL,
   viewed_at INT,
   skipped_at INT,
   bookmarked_at INT,
@@ -172,62 +97,62 @@ CREATE TABLE user_item (
   reported_at INT,
   report_reason TEXT,
   FOREIGN KEY(user_id) REFERENCES user(id),
-  FOREIGN KEY(item_id) REFERENCES item_base(id)
+  FOREIGN KEY(item_id) REFERENCES item(id)
 ) STRICT;
 
 CREATE TABLE digest (
-  id BLOB PRIMARY KEY,
-  user_id BLOB,
-  sent_at INT,
-  subject_item_id BLOB,
+  id BLOB PRIMARY KEY NOT NULL,
+  user_id BLOB NOT NULL,
+  sent_at INT NOT NULL,
+  subject_id BLOB,
   ad_id BLOB,
   bulk_send_id BLOB,
   FOREIGN KEY(user_id) REFERENCES user(id),
-  FOREIGN KEY(subject_item_id) REFERENCES item_base(id),
+  FOREIGN KEY(subject_id) REFERENCES item(id),
   FOREIGN KEY(ad_id) REFERENCES ad(id),
   FOREIGN KEY(bulk_send_id) REFERENCES bulk_send(id)
 ) STRICT;
 
 CREATE TABLE digest_item (
-  id BLOB PRIMARY KEY,
-  digest_id BLOB,
-  item_id BLOB,
-  kind INT CHECK (kind IN (0, 1)), -- :icymi (0), :discover (1)
+  id BLOB PRIMARY KEY NOT NULL,
+  digest_id BLOB NOT NULL,
+  item_id BLOB NOT NULL,
+  kind INT NOT NULL CHECK (kind IN (0, 1)), -- :icymi (0), :discover (1)
   FOREIGN KEY(digest_id) REFERENCES digest(id),
-  FOREIGN KEY(item_id) REFERENCES item_base(id)
+  FOREIGN KEY(item_id) REFERENCES item(id)
 ) STRICT;
 
 CREATE TABLE bulk_send (
-  id BLOB PRIMARY KEY,
-  sent_at INT,
-  payload_size INT,
-  mailersend_id TEXT,
-  digests BLOB -- Serialized list of digest UUIDs
+  id BLOB PRIMARY KEY NOT NULL,
+  sent_at INT NOT NULL,
+  payload_size INT NOT NULL,
+  mailersend_id TEXT NOT NULL,
+  digests BLOB NOT NULL -- [:vector :uuid]
 ) STRICT;
 
 CREATE TABLE reclist (
-  id BLOB PRIMARY KEY,
-  user_id BLOB,
-  created_at INT,
-  clicked BLOB, -- Serialized set of UUIDs
+  id BLOB PRIMARY KEY NOT NULL,
+  user_id BLOB NOT NULL,
+  created_at INT NOT NULL,
+  clicked BLOB NOT NULL, -- [:set :uuid]
   FOREIGN KEY(user_id) REFERENCES user(id)
 ) STRICT;
 
 CREATE TABLE skip (
-  id BLOB PRIMARY KEY,
-  reclist_id BLOB,
-  item_id BLOB,
+  id BLOB PRIMARY KEY NOT NULL,
+  reclist_id BLOB NOT NULL,
+  item_id BLOB NOT NULL,
   FOREIGN KEY(reclist_id) REFERENCES reclist(id),
-  FOREIGN KEY(item_id) REFERENCES item_base(id)
+  FOREIGN KEY(item_id) REFERENCES item(id)
 ) STRICT;
 
 CREATE TABLE ad (
-  id BLOB PRIMARY KEY,
-  user_id BLOB,
-  approve_state INT CHECK (approve_state IN (0, 1, 2)), -- :pending (0), :approved (1), :rejected (2)
-  updated_at INT,
-  balance INT,
-  recent_cost INT,
+  id BLOB PRIMARY KEY NOT NULL,
+  user_id BLOB NOT NULL,
+  approve_state INT NOT NULL CHECK (approve_state IN (0, 1, 2)), -- :pending (0), :approved (1), :rejected (2)
+  updated_at INT NOT NULL,
+  balance INT NOT NULL,
+  recent_cost INT NOT NULL,
   bid INT,
   budget INT,
   url TEXT,
@@ -239,51 +164,51 @@ CREATE TABLE ad (
   customer_id TEXT,
   session_id TEXT,
   payment_method TEXT,
-  card_details BLOB, -- Serialized map
+  card_details BLOB, -- [:map [:brand :string] [:last4 :string] [:exp_year :int] [:exp_month :int]]
   FOREIGN KEY(user_id) REFERENCES user(id)
 ) STRICT;
 
 CREATE TABLE ad_click (
-  id BLOB PRIMARY KEY,
-  user_id BLOB,
-  ad_id BLOB,
-  created_at INT,
-  cost INT,
-  source INT CHECK (source IN (0, 1)), -- :web (0), :email (1)
+  id BLOB PRIMARY KEY NOT NULL,
+  user_id BLOB NOT NULL,
+  ad_id BLOB NOT NULL,
+  created_at INT NOT NULL,
+  cost INT NOT NULL,
+  source INT NOT NULL CHECK (source IN (0, 1)), -- :web (0), :email (1)
   FOREIGN KEY(user_id) REFERENCES user(id),
   FOREIGN KEY(ad_id) REFERENCES ad(id)
 ) STRICT;
 
 CREATE TABLE ad_credit (
-  id BLOB PRIMARY KEY,
-  ad_id BLOB,
-  source INT CHECK (source IN (0, 1)), -- :charge (0), :manual (1)
-  amount INT,
-  created_at INT,
+  id BLOB PRIMARY KEY NOT NULL,
+  ad_id BLOB NOT NULL,
+  source INT NOT NULL CHECK (source IN (0, 1)), -- :charge (0), :manual (1)
+  amount INT NOT NULL,
+  created_at INT NOT NULL,
   charge_status INT CHECK (charge_status IN (0, 1, 2)), -- :pending (0), :confirmed (1), :failed (2)
   FOREIGN KEY(ad_id) REFERENCES ad(id)
 ) STRICT;
 
 CREATE TABLE mv_sub (
-  id BLOB PRIMARY KEY,
-  sub_id BLOB,
-  affinity_low BLOB, -- Serialized double
-  affinity_high BLOB, -- Serialized double
+  id BLOB PRIMARY KEY NOT NULL,
+  sub_id BLOB NOT NULL,
+  affinity_low REAL,
+  affinity_high REAL,
   last_published INT,
   unread INT,
-  read INT,
-  FOREIGN KEY(sub_id) REFERENCES sub_base(id)
+  n_read INT,
+  FOREIGN KEY(sub_id) REFERENCES sub(id)
 ) STRICT;
 
 CREATE TABLE mv_user (
-  id BLOB PRIMARY KEY,
-  user_id BLOB,
+  id BLOB PRIMARY KEY NOT NULL,
+  user_id BLOB NOT NULL,
   current_item_id BLOB,
   FOREIGN KEY(user_id) REFERENCES user(id),
-  FOREIGN KEY(current_item_id) REFERENCES item_base(id)
+  FOREIGN KEY(current_item_id) REFERENCES item(id)
 ) STRICT;
 
 CREATE TABLE deleted_user (
-  id BLOB PRIMARY KEY,
-  email_username_hash TEXT
+  id BLOB PRIMARY KEY NOT NULL,
+  email_username_hash TEXT NOT NULL
 ) STRICT;
